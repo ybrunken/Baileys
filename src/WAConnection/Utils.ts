ef2d39613f057c1d148b0cfa456896a0ef413786
Yannick Brunken <yannick.brunken@yahoo.de>
2022-01-04 21:09:07 +0100

    Update displayed OS name

--- src/WAConnection/Utils.ts
+++ src/WAConnection/Utils.ts
@@ -25,7 +25,7 @@ const platformMap = {
 export const Browsers = {
     ubuntu: browser => ['Ubuntu', browser, '18.04'] as [string, string, string],
     macOS: browser => ['Mac OS', browser, '10.15.3'] as [string, string, string],
-    baileys: browser => ['Baileys', browser, '3.0'] as [string, string, string],
+    baileys: browser => ['Wear', browser, '3.0'] as [string, string, string],
     /** The appropriate browser based on your OS & release */
     appropriate: browser => [ platformMap [platform()] || 'Ubuntu', browser, release() ] as [string, string, string]
 }
